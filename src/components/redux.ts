8bdb064538fd55321453da89687b9583af1bce9b
Olesya Shalay <olesyakublickaya@yandex.ru>
2024-06-03 14:14:22 +0300

    localStorage in Redux

--- src/components/redux.ts
+++ src/components/redux.ts
@@ -1,19 +1,54 @@
-import { combineReducers, legacy_createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { counterReducer } from "./counter/counter-reducer";
 //import { settingsValueReducer } from "./settings/settingsValue-reducer";
 import { settingsReducer } from "./settings/settings-reducer";
-import { commonReducer } from "./common-reducer";
-
+import { CommonStateType, commonReducer } from "./common-reducer";
 
 const rootReducer = combineReducers({
   // counter: counterReducer,
   // settings: settingsReducer,
-  commonData: commonReducer
- 
+  commonData: commonReducer,
 });
 
-export const store = legacy_createStore(rootReducer);
+const localMin = localStorage.getItem("min");
+const localMax = localStorage.getItem("max");
+
+let preloadedMin = localMin ? JSON.parse(localMin) : 0;
+let preloadedMax = localMax ? JSON.parse(localMax) : 5;
+
+
+let preloadedState: any = {
+  maxValue: preloadedMax,
+  minValue: preloadedMin,
+  valuesSetWarning: true,
+  incorrectValue: false,
+  number: 0,
+};
+
+
+if (localMin && localMax) {
+  preloadedMin = JSON.parse(localMin);
+  preloadedMax = JSON.parse(localMax);
+}
+
+export const store = legacy_createStore(
+  rootReducer,
+  {
+    commonData: preloadedState,
+  },
+  applyMiddleware()
+);
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 
+store.subscribe(() => {
+  localStorage.setItem(
+    "min",
+    JSON.stringify(store.getState().commonData.minValue)
+  );
+  localStorage.setItem(
+    "max",
+    JSON.stringify(store.getState().commonData.maxValue)
+  );
+});
 //@ts-ignore
 window.store = store;
